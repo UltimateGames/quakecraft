484bcd3b7c9a94916c58b176cea9984036eefa26
Taaral <nlatrick@hotmail.com>
2013-11-22 09:29:42 -0500

    Api change

--- src/main/java/com/greatmancode/quakecraft/QuakeCraft.java
+++ src/main/java/com/greatmancode/quakecraft/QuakeCraft.java
@@ -45,7 +45,7 @@ public class QuakeCraft extends GamePlugin {
     private static final int LEVEL_MIN = 0;
 
     @Override
-    public Boolean loadGame(UltimateGames ultimateGames, Game game) {
+    public boolean loadGame(UltimateGames ultimateGames, Game game) {
         this.ultimateGames = ultimateGames;
         this.game = game;
         ItemStack railgunItem = new ItemStack(Material.BLAZE_ROD);
@@ -61,38 +61,38 @@ public class QuakeCraft extends GamePlugin {
     public void unloadGame() {}
 
     @Override
-    public Boolean reloadGame() {
+    public boolean reloadGame() {
         return true;
     }
 
     @Override
-    public Boolean stopGame() {
+    public boolean stopGame() {
         return true;
     }
 
     @Override
-    public Boolean loadArena(Arena arena) {
+    public boolean loadArena(Arena arena) {
         ultimateGames.addAPIHandler("/" + game.getName() + "/" + arena.getName(), new QuakecraftWebHandler(ultimateGames, arena));
         return true;
     }
 
     @Override
-    public Boolean unloadArena(Arena arena) {
+    public boolean unloadArena(Arena arena) {
         return true;
     }
 
     @Override
-    public Boolean isStartPossible(Arena arena) {
+    public boolean isStartPossible(Arena arena) {
         return arena.getStatus() == ArenaStatus.OPEN;
     }
 
     @Override
-    public Boolean startArena(Arena arena) {
+    public boolean startArena(Arena arena) {
         return true;
     }
 
     @Override
-    public Boolean beginArena(Arena arena) {
+    public boolean beginArena(Arena arena) {
         ultimateGames.getCountdownManager().createEndingCountdown(arena, 1200, false);
 
         ArenaScoreboard scoreBoard = ultimateGames.getScoreboardManager().createArenaScoreboard(arena, "Gibs");
@@ -143,22 +143,22 @@ public class QuakeCraft extends GamePlugin {
     }
 
     @Override
-    public Boolean resetArena(Arena arena) {
+    public boolean resetArena(Arena arena) {
         return true;
     }
 
     @Override
-    public Boolean openArena(Arena arena) {
+    public boolean openArena(Arena arena) {
         return true;
     }
 
     @Override
-    public Boolean stopArena(Arena arena) {
+    public boolean stopArena(Arena arena) {
         return true;
     }
 
     @Override
-    public Boolean addPlayer(Player player, Arena arena) {
+    public boolean addPlayer(Player player, Arena arena) {
         if (arena.getPlayers().size() >= arena.getMinPlayers() && !ultimateGames.getCountdownManager().hasStartingCountdown(arena)) {
             ultimateGames.getCountdownManager().createStartingCountdown(arena, ultimateGames.getConfigManager().getGameConfig(game).getConfig().getInt("CustomValues.StartWaitTime"));
         }
@@ -185,7 +185,7 @@ public class QuakeCraft extends GamePlugin {
 
     @SuppressWarnings("deprecation")
     @Override
-    public Boolean addSpectator(Player player, Arena arena) {
+    public boolean addSpectator(Player player, Arena arena) {
         ultimateGames.getSpawnpointManager().getSpectatorSpawnPoint(arena).teleportPlayer(player);
         for (PotionEffect potionEffect : player.getActivePotionEffects()) {
             player.removePotionEffect(potionEffect.getType());
